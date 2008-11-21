0360fe4f9383a6d11b5f1c85405ed7ebbec57955
luxiudi <luxiudi@c3632fb2-9a14-11dd-a29e-e3fa11317253>
2008-11-21 20:45:49 +0000

    made a copy

---
+++ branches/no-roboticsstudio/CS266/SimCon/Controller/Algorithms/Algorithm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+
+namespace CS266.SimCon.Controller
+{
+    public class Algorithm
+    {
+
+        public Robot Robot;
+
+        public Algorithm(Robot robot)
+        {
+            Robot = robot;
+            // What else?
+        }
+    }
+}
