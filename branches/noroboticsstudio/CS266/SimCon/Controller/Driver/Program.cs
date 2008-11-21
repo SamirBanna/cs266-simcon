a8876285b8144de1b271755f4b29b29ce01996eb
luxiudi <luxiudi@c3632fb2-9a14-11dd-a29e-e3fa11317253>
2008-11-21 20:49:59 +0000

    made a copy

---
+++ branches/noroboticsstudio/CS266/SimCon/Controller/Driver/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+using CS266.SimCon.Controller.WorldInputInterfaces;
+//using Robotics.SimulationService;
+using CS266.SimCon.Controller.Driver;
+
+namespace CS266.SimCon.Controller
+{
+    public static class Program
+    {
+
+        static ControlLoop controlLoop;
+        static WorldInputInterface wip;
+
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            RobotTest.Run();
+            //Driver.Driver.Run();
+        }
+    }
+}
---
+++ branches/noroboticsstudio/CS266/SimCon/Controller/PhysObject/Food.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+
+namespace CS266.SimCon.Controller
+{
+    public class Food : PhysObject
+    {
+        public Food(int id, String name, Coordinates location, float orientation, float width, float height) :
+            base(id, name, location, orientation, width, height)
+        {
+            //TODO Add food-specific constructor parameters
+        }
+    }
+}
