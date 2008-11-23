dcbac51e440543390b07826b93f2f9737bd427c7
luxiudi <luxiudi@c3632fb2-9a14-11dd-a29e-e3fa11317253>
2008-11-23 17:34:30 +0000

    forgot to add files

---
+++ CS266/SimCon/Controller/Algorithms/WalkStraight.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS266.SimCon.Controller
+{
+    public class WalkStraight:Algorithm
+    {
+        public WalkStraight(Robot r) : base(r) 
+        { 
+        
+        }
+
+        public override void Execute()
+        {
+            robot.MoveForward(100);                
+        }
+    }
+}
