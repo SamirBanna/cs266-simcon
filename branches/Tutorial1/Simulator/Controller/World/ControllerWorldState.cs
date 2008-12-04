840c4514a0dde95b63b9a9cb81c27f3cf0037894
kpozin <kpozin@c3632fb2-9a14-11dd-a29e-e3fa11317253>
2008-12-04 19:32:08 +0000

    Started out working on the grid. Key changes in ControllerWorldState and GridData. Can't compile because of missing simulator classes.

--- branches/Tutorial1/Simulator/Controller/World/ControllerWorldState.cs
+++ branches/Tutorial1/Simulator/Controller/World/ControllerWorldState.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 
+using CS266.SimCon.Controller.World;
+
 namespace CS266.SimCon.Controller
 {
     public class ControllerWorldState
@@ -9,11 +11,33 @@ namespace CS266.SimCon.Controller
         public List<Robot> robots;
         public List<PhysObject> physobjects;
 
+        public double WorldWidth;
+        public double WorldHeight;
+
+        public int NumSquaresX;
+        public int NumSquaresY;
+
+        public GridData[][] Grid;
+
         public ControllerWorldState(List<Robot> robots, List<PhysObject> physobjects)
         {
             this.robots = robots;
             this.physobjects = physobjects;
         }
+
+        public ControllerWorldState(List<Robot> robots, List<PhysObject> physObjects,
+            double worldWidth, double worldHeight, int numSquaresX, int numSquaresY)
+            : this(robots, physobjects)
+        {
+            WorldHeight = worldHeight;
+            WorldWidth = worldWidth;
+            NumSquaresX = numSquaresX;
+            NumSquaresY = numSquaresY;
+
+            Grid = new GridData(false)[NumSquaresX][NumSquaresY];
+        }
+
+
 
     }
 }
