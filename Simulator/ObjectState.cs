34b504e3f05f5242eb42d76735cdd3ce4ae85785
kpozin <kpozin@c3632fb2-9a14-11dd-a29e-e3fa11317253>
2008-12-06 19:59:43 +0000

    Refactored, renamed the simulator namespaces, split out all the simulator classes, got it to work. Please see http://code.google.com/p/cs266-simcon/wiki/SolutionSettingsForRoboticsService for instructions on setting up the Debug/run settings I now have the service manifest file and "Game Paremeters.txt" automatically copied from the source directory to the output bin folder during the build. The one thing that still needs to be fixed is the location of the file read by the WorldStateParse -- I didn't have time to figure this out. Contact me with any questions -- Konstantin.

---
+++ Simulator/ObjectState.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.Robotics.PhysicalModel;
+
+namespace CS266.SimCon.Simulator
+{
+    public class ObjectState
+    {
+        public string name;
+        public string type;
+        public Vector3 position;
+        public float speed;
+        public float degreesfromx;
+        public Vector3 dimension;
+
+
+        public ObjectState(string name, string type, float[] position,
+        float[] orientation, float[] velocity, float[] dimension)
+        {
+            this.name = name; // for all objects
+            this.type = type; //type is either 'robot', 'obstacle', 'food'
+            // this.position = position;
+            this.position.X = position[0]; //for all objects
+            this.position.Y = position[1]; //for all objects
+            this.position.Z = position[2]; //for all objects
+
+            float x = orientation[0]; //  for robots and obstacles, if it's not a robot, just set orientation to [1,0,0]
+            float z = orientation[2]; //radians = Math.Atan(z/x);
+            double radians = Math.Atan(z / x);
+            double angle = radians * (180 / Math.PI);
+
+            this.degreesfromx = (float)angle;  //only for robots
+
+            this.speed = (float)Math.Sqrt(Math.Pow(velocity[0], 2) + Math.Pow(velocity[2], 2)); //only for robots
+
+            // this.dimension = dimension; //dimension is <x,y,z> (y is into the air, z is top down, x is left right),
+            //specified only for obstacles, otherwise set it to [0,0,0]
+            this.dimension.X = dimension[0];
+            this.dimension.Y = dimension[1];
+            this.dimension.Z = dimension[2];
+
+        }
+    }
+
+}
---
+++ Simulator/RobotActions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS266.SimCon.Simulator
+{
+    public class RobotActions
+    {
+        //PHASE 1
+        //public RobotAction[] actions;
+        public List<RobotAction> actions;
+        public int size;
+        public RobotActions(List<RobotAction> acts)
+        {
+            this.actions = acts;
+            this.size = acts.Count;
+        }
+        //Newly Added
+        public RobotActions()
+        {
+            this.actions = new List<RobotAction>();
+            this.size = actions.Count;
+        }
+        //Newly Added
+        public void Add(RobotAction act)
+        {
+            this.actions.Add(act);
+            this.size = this.actions.Count;
+        }
+    }
+}
