5815fc80c7cd6f036ccc53ef368abf437e3edab6
luxiudi <luxiudi@c3632fb2-9a14-11dd-a29e-e3fa11317253>
2008-12-12 19:35:37 +0000

    Updates from Andrew.

--- Controller/WorldInputInterfaces/SimulatorInputInterface.cs
+++ Controller/WorldInputInterfaces/SimulatorInputInterface.cs
@@ -8,8 +8,8 @@ namespace CS266.SimCon.Controller.WorldInputInterfaces
     public class SimulatorInputInterface:WorldInputInterface
     {
         CS266.SimCon.Simulator.OurSimulator os;
-        public double worldWidth = 200;
-        public double worldHeight = 90;
+        public double worldWidth = 20;
+        public double worldHeight = 9;
 
         public SimulatorInputInterface(CS266.SimCon.Simulator.OurSimulator os)
             : base()
